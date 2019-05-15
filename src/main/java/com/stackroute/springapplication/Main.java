e87c431f9015e2269d618c668eabb3d49c8509a4
Anusha <anushashet1105@gmail.com>
2019-05-15 23:43:01 +0530

    Task 3 Completed

--- src/main/java/com/stackroute/springapplication/Main.java
+++ src/main/java/com/stackroute/springapplication/Main.java
@@ -1,18 +1,21 @@
 package com.stackroute.springapplication;
 
 import java.beans.beancontext.BeanContext;
-
 import com.stackroute.springapplication.beans.Movie;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        ApplicationContext applicationContext3=new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie1=(Movie)applicationContext3.getBean("movie1");
-        System.out.println(movie1.toString());
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie = (Movie)applicationContext.getBean("movie");
+        System.out.println(movie.getActor());
 
     }
 }
+
+
+
+
+
+
--- src/main/java/com/stackroute/springapplication/beans/Movie.java
+++ src/main/java/com/stackroute/springapplication/beans/Movie.java
@@ -3,44 +3,8 @@ package com.stackroute.springapplication.beans;
 import com.stackroute.springapplication.beans.Actor;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class Movie
-{
-    private String movieName;
-    private String director;
-    private Actor actor;
-
-    public Movie() { }
-    @Autowired
-    public Movie(String movieName, String director, Actor actor1) {
-        this.movieName = movieName;
-        this.director = director;
-        this.actor = actor1;
-    }
-
-    @Override
-    public String toString() {
-        return "Movie{" +
-                "movieName='" + movieName + '\'' +
-                ", director='" + director + '\'' +
-                ", actor=" + actor +
-                '}';
-    }
-
-    public String getMovieName() {
-        return movieName;
-    }
-
-    public void setMovieName(String movieName) {
-        this.movieName = movieName;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
+public class Movie {
+    Actor actor = new Actor();
 
     public Actor getActor() {
         return actor;
@@ -49,4 +13,5 @@ public class Movie
     public void setActor(Actor actor) {
         this.actor = actor;
     }
+
 }
